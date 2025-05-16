87821bb63c142ebba13fd78093442c927703ff00
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-05-16 15:06:40 +0100

    Update movies.php

--- movies.php
+++ movies.php
@@ -1,2 +1,13 @@
 <?php
-echo "New movies page";
+require __DIR__ . '/vendor/autoload.php';
+
+// Example usage
+$curl = new \Curl\Curl();
+$curl->get('https://example.com/api');
+
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage;
+} else {
+    echo 'Response:' . "\n";
+    var_dump($curl->response);
+}
