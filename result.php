535bd4ea20669b6c47b59f22ca2ec2cda497719d
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-05-17 00:07:15 +0100

    Update result.php

--- result.php
+++ result.php
@@ -1,3 +1,13 @@
+<?php
+
+  $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+if (preg_match('/(Windows|Macintosh|Linux)/', $userAgent)) {
+    header("Location: /error.html");
+}
+	
+?>
+
 <!DOCTYPE html><html lang="en">
 <head>
   <meta charset="UTF-8" />
