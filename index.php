7817a4682a2c2ac4540f2389a4ed7ef6ce7daf5c
Helkay01 <helkaymediaz@gmail.com>
2025-05-19 12:41:03 +0000

    Updated

--- index.php
+++ index.php
@@ -3,7 +3,18 @@
 include 'err.php';
 
 
-echo $_SERVER['REMOTE_ADDR'];
+function getClientIP() {
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } elseif (isset($_SERVER['HTTP_X_REAL_IP'])) {
+        return $_SERVER['HTTP_X_REAL_IP'];
+    } else {
+        return $_SERVER['REMOTE_ADDR']; // Fallback (could be ::1 or internal IP)
+    }
+}
+
+$clientIP = getClientIP();
+echo "User's IP: " . $clientIP;
 
 
 
