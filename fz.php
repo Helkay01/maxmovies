a6abed3f7564c77af91516efcf5f08489f3ce010
Helkay01 <helkaymediaz@gmail.com>
2025-05-24 20:27:28 +0000

    Updated

--- fz.php
+++ fz.php
@@ -1,201 +1,432 @@
 <?php
+require "vendor/autoload.php";
+
+use Curl\Curl;
+
+$title = $_GET['t'];
+
+$year = $_GET['y'];
 
 
-    
-?>
+$no_space = str_replace(" ", "", $title);
 
-
-
-
-<title>Watch <?php echo $_GET['t']; ?></title>
-
-<script src="vplayer.min.js"></script>
-
-
-
-
+$title_array = explode(" ", $title);
 
 
 
 
+//page5
+function nextPage5($link4) {
+   	$curl55 = new Curl();
+   	$cookieFile = 'cookies.txt';
 
+   	// Enable cookie handling
+   	$curl55->setOpt(CURLOPT_COOKIEJAR, $cookieFile); // Save cookies to a file
+	$curl55->setOpt(CURLOPT_COOKIEFILE, $cookieFile);
 
+	$curl55->disableTimeout();
+	
+   	$curl55->get("https://fzmovies.net/".$link4);
 
-<script>
-/*
+	if($curl55->getHttpStatusCode() === 200) {
+		$html5 = $curl55->response;
 
-async function loadVASTAd(vastUrl) {
-    const response = await fetch(vastUrl);
-    const xml = await response.text();
-    return xml;
+      	$dom5 = new DOMDocument();
+     	@$dom5->loadHTML($html5);
+
+     	$inp = $dom5->getElementsByTagName("input")->item(0);
+
+		echo $inp->getAttribute("value");
+	
+   	}
+
 }
 
-function parseVAST(xml) {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xml, "text/xml");
-    const ad = xmlDoc.getElementsByTagName("MediaFile")[0];
-    return ad ? ad.textContent : null;
-}
 
-async function playAd() {
-    const vastUrl = 'https://knownamount.com/dmmDFWzXd.GTNzvUZEG-UE/pesm/9/ujZFUJl-kfPuTgU/2jOiDBcm1TM/TAYHtmNhT/Yd4-N/zGUbxFNby/ZNsGaMWI1HptdBDd0Gxe'; // Replace with your VAST URL
-    const vastXML = await loadVASTAd(vastUrl);
-    const adUrl = parseVAST(vastXML);
-    
-    if (adUrl) {
-        const videoElement = document.querySelector('video');
-        videoElement.src = adUrl; // Set the ad video source
-        videoElement.play(); // Play the ad
+
+
+//Page4
+function nextPage4($link3) {
+	$curl44 = new Curl();
+
+	$lky2 = str_replace("download.php?downloadkey=", "", $link3);
+
+	$cookieFile = 'cookies.txt';
+
+    // Enable cookie handling
+    $curl44->setOpt(CURLOPT_COOKIEJAR, $cookieFile); // Save cookies to a file
+    $curl44->setOpt(CURLOPT_COOKIEFILE, $cookieFile);
+
+	$curl44->disableTimeout();
+	
+	$curl44->get("https://fzmovies.net/download.php", [
+      "downloadkey" => $lky2,
+    ]);
+
+
+
+	
+	if($curl44->getHttpStatusCode() === 200) {
+		$curl44->close();
+		$html4 = $curl44->response;
+
+				
+		$dom4 = new DOMDocument();
+		@$dom4->loadHTML($html4);
+
 		
-		videoElement.onplaying = function() {		
-			$("small#title").html("Please wait, Video will play after the ad.");
-			$("small#title").css("color", "lightblue");
-			
-			$(".vp-progress div").show();
-		}			
-		 			
-        videoElement.onended = function() {
+		$all_link4 = $dom4->getElementsByTagName("a");
 		
-			var title = videoElement.dataset.title;
-			
-			$("small#title").html(title);
-			
-			$(".vp-progress div").hide();
-			        
-			videoElement.src = videoElement.dataset.video; // Your main video source
-			videoElement.play();
-			
-			videoElement.onplaying = function() {
-				$(".vp-progress div").show();
+		foreach($all_link4 as $newLinks4) {
+			$fzlink4 = $newLinks4->getAttribute("href");
+
+			if(str_contains($fzlink4, "dlink")) {
+				nextPage5($fzlink4);			
 			}
+		}
+		
+		
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+//Page3
+function nextPage3($link2) {
+
+
+	$lky = str_replace("download1.php?downloadoptionskey=", "", $link2);
+
+	$curl33 = new Curl();
+
+	$cookieFile = 'cookies.txt';
+
+	// Enable cookie handling
+	$curl33->setOpt(CURLOPT_COOKIEJAR, $cookieFile); // Save cookies to a file
+	$curl33->setOpt(CURLOPT_COOKIEFILE, $cookieFile); // Load cookies from the file
+
+
+	$curl33->disableTimeout();
+	
+    $curl33->get("https://fzmovies.net/download1.php", [
+	 "downloadoptionskey" => $lky,
+	]);
+
+
+	if($curl33->getHttpStatusCode() === 200) {
+		$curl33->close();
+		$html3 = $curl33->response;
+		
+		
+		$dom3 = new DOMDocument();
+		@$dom3->loadHTML($html3);
+
+
+		$all_link3 = $dom3->getElementsByTagName("a");
+		
+		foreach($all_link3 as $newLinks3) {
+			$fzlink3 = $newLinks3->getAttribute("href");
+
+
+			if(str_contains($fzlink3, "downloadkey")) {
+				nextPage4($fzlink3);
+			}
+		}
+		
+		
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+//Page2
+function nextPage2($link1) {
+
+	$curl22 = new Curl();
+
+	$cookieFile = 'cookies.txt';
+
+	// Enable cookie handling
+	$curl22->setOpt(CURLOPT_COOKIEJAR, $cookieFile); // Save cookies to a file
+	$curl22->setOpt(CURLOPT_COOKIEFILE, $cookieFile); // Load cookies from the file
+	
+	$curl22->disableTimeout();
+	
+	$curl22->get("https://fzmovies.net/".$link1);
+
+	if($curl22->getHttpStatusCode() === 200) {
+		$curl22->close();
+		$html2 = $curl22->response;
+
+
+		$dom2 = new DOMDocument();
+		@$dom2->loadHTML($html2);
+
+
+		$all_link2 = $dom2->getElementsByTagName("a");
+
+		foreach($all_link2 as $newLinks) {
+			$fzlink2 = $newLinks->getAttribute("href");
+			if(str_contains($fzlink2, "download1.php") !== false) {
+				nextPage3($fzlink2);
+			}
+
+		}
+
+	}
+
+
+
+}
+
+
+
+
+
+
+
+//Curl Start
+
+$url = 'https://fzmovies.net/csearch.php';
+
+// Form data to be submitted
+$formData = array(
+    'searchname' => $title,
+);
+
+$curl = new Curl();
+
+$cookieFile = 'cookies.txt';
+
+// Enable cookie handling
+$curl->setOpt(CURLOPT_COOKIEJAR, $cookieFile); // Save cookies to a file
+$curl->setOpt(CURLOPT_COOKIEFILE, $cookieFile); // Load cookies from the file
+
+$curl->disableTimeout();
+	
+$curl->get($url, $formData);
+
+
+if($curl->getHttpStatusCode() === 200) {
+
+	$curl->close();
+
+	$html = $curl->response;
+
+	$dom = new DOMDocument();
+	@$dom->loadHTML($html);
+
+	$xpath = new DOMXPath($dom);
+
+	// Define the class name you want to select
+	$className = 'mainbox';
+
+	// Use XPath query to select elements by class name
+	$elements = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $className ')]");
+
+	for($e = 0; $e < count($elements); $e++) {
+		$one = $elements->item($e);
+		$lk = $one->getElementsByTagName("a")->item(0);
+		$sm = $one->getElementsByTagName("small")->item(1);
+
+		$fzlink = $lk->getAttribute('href');
+		$yr = $sm->nodeValue;
+		$yr1 = str_replace("(", "", $yr);
+		$yr2 = str_replace(")", "", $yr1);
+
+		
+		if(count($title_array) === 1) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0])) || str_contains($fzlink, strtolower($title_array[0]))) {
+				if(str_contains($yr, $year)) {
+			      	nextPage2($fzlink);
+			   	}
+			}
+		}
+		else if(count($title_array) === 2) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0])) || str_contains($fzlink, strtolower($title_array[0]))) {
+				if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1])) || str_contains($fzlink, strtolower($title_array[1]))) {	
+					if(str_contains($yr, $year)) {
+						nextPage2($fzlink);
+					}
+				}
+			}
+		}
+		else if(count($title_array) === 3) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0]))  || str_contains($fzlink, strtolower($title_array[0]))) {
+			    if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1]))  || str_contains($fzlink, strtolower($title_array[1]))) {
+			    	if(str_contains($fzlink, ucwords($title_array[2])) || str_contains($fzlink, strtoupper($title_array[2]))  || str_contains($fzlink, strtolower($title_array[2]))) {
+			    		if(str_contains($yr, $year)) {
+			    			nextPage2($fzlink);
+			    		}
+			    	}
+			    }
+			}
+		}
+		else if(count($title_array) === 4) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0]))  || str_contains($fzlink, strtolower($title_array[0]))) {
+			    if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1]))  || str_contains($fzlink, strtolower($title_array[1]))) {
+			    	if(str_contains($fzlink, ucwords($title_array[2])) || str_contains($fzlink, strtoupper($title_array[2]))  || str_contains($fzlink, strtolower($title_array[2]))) {
+					    if(str_contains($fzlink, ucwords($title_array[3])) || str_contains($fzlink, strtoupper($title_array[3]))  || str_contains($fzlink, strtolower($title_array[3]))) {
+			    			if(str_contains($yr, $year)) {
+			    				nextPage2($fzlink);
+			    			}
+			    		}
+			    	}
+			    }
+			}
+		}
+		else if(count($title_array) === 5) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0]))  || str_contains($fzlink, strtolower($title_array[0]))) {
+			    if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1]))  || str_contains($fzlink, strtolower($title_array[1]))) {
+			    	if(str_contains($fzlink, ucwords($title_array[2])) || str_contains($fzlink, strtoupper($title_array[2]))  || str_contains($fzlink, strtolower($title_array[2]))) {
+					    if(str_contains($fzlink, ucwords($title_array[3])) || str_contains($fzlink, strtoupper($title_array[3]))  || str_contains($fzlink, strtolower($title_array[3]))) {
+			    		    if(str_contains($fzlink, ucwords($title_array[4])) || str_contains($fzlink, strtoupper($title_array[4]))  || str_contains($fzlink, strtolower($title_array[4]))) {	
+			    				if(str_contains($yr, $year)) {
+			    					nextPage2($fzlink);
+			    				}
+			    			}
+			    		}
+			    	}
+			    }
+			}
+		}
+		else if(count($title_array) === 6) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0]))  || str_contains($fzlink, strtolower($title_array[0]))) {
+			    if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1]))  || str_contains($fzlink, strtolower($title_array[1]))) {
+			    	if(str_contains($fzlink, ucwords($title_array[2])) || str_contains($fzlink, strtoupper($title_array[2]))  || str_contains($fzlink, strtolower($title_array[2]))) {
+					    if(str_contains($fzlink, ucwords($title_array[3])) || str_contains($fzlink, strtoupper($title_array[3]))  || str_contains($fzlink, strtolower($title_array[3]))) {
+			    		    if(str_contains($fzlink, ucwords($title_array[4])) || str_contains($fzlink, strtoupper($title_array[4]))  || str_contains($fzlink, strtolower($title_array[4]))) {	
+			    			    if(str_contains($fzlink, ucwords($title_array[5])) || str_contains($fzlink, strtoupper($title_array[5]))  || str_contains($fzlink, strtolower($title_array[5]))) {		
+			    					if(str_contains($yr, $year)) {
+			    						nextPage2($fzlink);
+			    					}
+			    				}	
+			    			}
+			    		}
+			    	}
+			    }
+			}
+		}
+		else if(count($title_array) === 7) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0]))  || str_contains($fzlink, strtolower($title_array[0]))) {
+			    if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1]))  || str_contains($fzlink, strtolower($title_array[1]))) {
+			    	if(str_contains($fzlink, ucwords($title_array[2])) || str_contains($fzlink, strtoupper($title_array[2]))  || str_contains($fzlink, strtolower($title_array[2]))) {
+					    if(str_contains($fzlink, ucwords($title_array[3])) || str_contains($fzlink, strtoupper($title_array[3]))  || str_contains($fzlink, strtolower($title_array[3]))) {
+			    		    if(str_contains($fzlink, ucwords($title_array[4])) || str_contains($fzlink, strtoupper($title_array[4]))  || str_contains($fzlink, strtolower($title_array[4]))) {	
+			    			    if(str_contains($fzlink, ucwords($title_array[5])) || str_contains($fzlink, strtoupper($title_array[5]))  || str_contains($fzlink, strtolower($title_array[5]))) {		
+			    					if(str_contains($fzlink, ucwords($title_array[6])) || str_contains($fzlink, strtoupper($title_array[6]))  || str_contains($fzlink, strtolower($title_array[6]))) {		
+			    						if(str_contains($yr, $year)) {
+			    							nextPage2($fzlink);
+			    						}
+			    					}	
+			    				}	
+			    			}
+			    		}
+			    	}
+			    }
+			}
+		}
+		else if(count($title_array) === 8) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0]))  || str_contains($fzlink, strtolower($title_array[0]))) {
+			    if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1]))  || str_contains($fzlink, strtolower($title_array[1]))) {
+			    	if(str_contains($fzlink, ucwords($title_array[2])) || str_contains($fzlink, strtoupper($title_array[2]))  || str_contains($fzlink, strtolower($title_array[2]))) {
+					    if(str_contains($fzlink, ucwords($title_array[3])) || str_contains($fzlink, strtoupper($title_array[3]))  || str_contains($fzlink, strtolower($title_array[3]))) {
+			    		    if(str_contains($fzlink, ucwords($title_array[4])) || str_contains($fzlink, strtoupper($title_array[4]))  || str_contains($fzlink, strtolower($title_array[4]))) {	
+			    			    if(str_contains($fzlink, ucwords($title_array[5])) || str_contains($fzlink, strtoupper($title_array[5]))  || str_contains($fzlink, strtolower($title_array[5]))) {		
+			    					if(str_contains($fzlink, ucwords($title_array[6])) || str_contains($fzlink, strtoupper($title_array[6]))  || str_contains($fzlink, strtolower($title_array[6]))) {		
+			    						if(str_contains($fzlink, ucwords($title_array[7])) || str_contains($fzlink, strtoupper($title_array[7]))  || str_contains($fzlink, strtolower($title_array[7]))) {				
+			    							if(str_contains($yr, $year)) {
+			    								nextPage2($fzlink);
+			    							}
+			    						}
+			    					}	
+			    				}	
+			    			}
+			    		}
+			    	}
+			    }
+			}
+		}
+		else if(count($title_array) === 9) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0]))  || str_contains($fzlink, strtolower($title_array[0]))) {
+			    if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1]))  || str_contains($fzlink, strtolower($title_array[1]))) {
+			    	if(str_contains($fzlink, ucwords($title_array[2])) || str_contains($fzlink, strtoupper($title_array[2]))  || str_contains($fzlink, strtolower($title_array[2]))) {
+					    if(str_contains($fzlink, ucwords($title_array[3])) || str_contains($fzlink, strtoupper($title_array[3]))  || str_contains($fzlink, strtolower($title_array[3]))) {
+			    		    if(str_contains($fzlink, ucwords($title_array[4])) || str_contains($fzlink, strtoupper($title_array[4]))  || str_contains($fzlink, strtolower($title_array[4]))) {	
+			    			    if(str_contains($fzlink, ucwords($title_array[5])) || str_contains($fzlink, strtoupper($title_array[5]))  || str_contains($fzlink, strtolower($title_array[5]))) {		
+			    					if(str_contains($fzlink, ucwords($title_array[6])) || str_contains($fzlink, strtoupper($title_array[6]))  || str_contains($fzlink, strtolower($title_array[6]))) {		
+			    						if(str_contains($fzlink, ucwords($title_array[7])) || str_contains($fzlink, strtoupper($title_array[7]))  || str_contains($fzlink, strtolower($title_array[7]))) {				
+			    							if(str_contains($fzlink, ucwords($title_array[8])) || str_contains($fzlink, strtoupper($title_array[8]))  || str_contains($fzlink, strtolower($title_array[8]))) {						
+			    								if(str_contains($yr, $year)) {
+			    									nextPage2($fzlink);
+			    								}
+			    							}	
+			    						}
+			    					}	
+			    				}	
+			    			}
+			    		}
+			    	}
+			    }
+			}
+		}
+		else if(count($title_array) === 10) {
+			if(str_contains($fzlink, ucwords($title_array[0])) || str_contains($fzlink, strtoupper($title_array[0]))  || str_contains($fzlink, strtolower($title_array[0]))) {
+			    if(str_contains($fzlink, ucwords($title_array[1])) || str_contains($fzlink, strtoupper($title_array[1]))  || str_contains($fzlink, strtolower($title_array[1]))) {
+			    	if(str_contains($fzlink, ucwords($title_array[2])) || str_contains($fzlink, strtoupper($title_array[2]))  || str_contains($fzlink, strtolower($title_array[2]))) {
+					    if(str_contains($fzlink, ucwords($title_array[3])) || str_contains($fzlink, strtoupper($title_array[3]))  || str_contains($fzlink, strtolower($title_array[3]))) {
+			    		    if(str_contains($fzlink, ucwords($title_array[4])) || str_contains($fzlink, strtoupper($title_array[4]))  || str_contains($fzlink, strtolower($title_array[4]))) {	
+			    			    if(str_contains($fzlink, ucwords($title_array[5])) || str_contains($fzlink, strtoupper($title_array[5]))  || str_contains($fzlink, strtolower($title_array[5]))) {		
+			    					if(str_contains($fzlink, ucwords($title_array[6])) || str_contains($fzlink, strtoupper($title_array[6]))  || str_contains($fzlink, strtolower($title_array[6]))) {		
+			    						if(str_contains($fzlink, ucwords($title_array[7])) || str_contains($fzlink, strtoupper($title_array[7]))  || str_contains($fzlink, strtolower($title_array[7]))) {				
+			    							if(str_contains($fzlink, ucwords($title_array[8])) || str_contains($fzlink, strtoupper($title_array[8]))  || str_contains($fzlink, strtolower($title_array[8]))) {						
+			    								if(str_contains($fzlink, ucwords($title_array[9])) || str_contains($fzlink, strtoupper($title_array[9]))  || str_contains($fzlink, strtolower($title_array[9]))) {								
+			    									if(str_contains($yr, $year)) {
+			    										nextPage2($fzlink);
+			    									}
+			    								}	
+			    							}	
+			    						}
+			    					}	
+			    				}	
+			    			}
+			    		}
+			    	}
+			    }
+			}
+		}
 			
-        }
-    } else {
-        console.error("No ad found in the VAST response.");
-        // Fallback to main video if no ad is found
-        const videoElement = document.querySelector('video');
-        videoElement.src = videoElement.dataset.video; // Your main video source
-        videoElement.play();
-        
-       	$(".vp-progress div").show();
-    }
+		
+
+			
+			
+	
+	
+	}
+
+
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    playAd();
-});
-
-*/
-
-
-
-
-
-
-
-$("document").ready(function() {
-    var vidm = document.querySelector("video");
-    
-    vidm.onplaying = function() {
-        $(".vp-progress div").show();
-    }
-    
-    
-    $('span#fs').hide();
-    
-    
-    
-    //Get movies
-    $.ajax({
-    	url: '/fz.php',
-    	data: {t: $("#mdeets").data("title"), y: $("#mdeets").data("year")},
-    	success: function(data) {
-    		alert(data);
-    	}
-    });
-    
-    
-     
-});
-    
-    
-</script>
-
-
-
-
-<style>
-div#vplayer {
-	box-shadow: 0 0 2px 0;
-	margin-top: -3px;
-}
-
-.vp-progress div {
-    display: none;
-}
-
-
-</style>
-
-
-
-
-
-
-
-	
-	<video style="" 
-		src=""
-		preload="metadata"
-		data-video=""
-		height="300px"
-		width="100%"
-		data-title="<?php echo strtoupper($_GET['t']); ?>"
-	>
-	</video>
-
-
-<br>
-<br>
-
-
-
-<div hidden data-year="<?php echo $_GET['t']; ?>"
-data-title="<?php echo $_GET['y']; ?>"
- id="mdeets"></div>
-
-<!--
-<div style="margin-top: 20px" id="similar">
-	
-	<div style="font-weight: bold; color: grey; font-size: 14px; margin-left: 6px;">
-		<label>You May Also Like</label>
-		
-		<div class="m-div" style="overflow: scrol;" id="trending-result">
-		
-		</div>
-	</div>
-	
-	
-	<div style="font-weight: bold; color: grey; font-size: 14px; margin-left: 6px;">
-		<label>New Movies</label>
-
-		<br>
-		
-		<div class="m-div" style="overflow: scrol;" id="search-result">
-		
-		</div>
-	</div>
-	
-
-	<br>
-	<br>
-	
-
-	
-</div>
-
--->
-
 
 
 
