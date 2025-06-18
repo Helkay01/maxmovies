ae1758c35af1a8bb36130c412cc3894bdc9189c3
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-06-18 09:12:27 +0100

    Update getip.php

--- getip.php
+++ getip.php
@@ -1,4 +1,12 @@
-<?php
-if(isset($_SERVER['REMOTE_ADDR'])) {
-echo $_SERVER['REMOTE_ADDR'];
+function getClientIP() {
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } elseif (isset($_SERVER['HTTP_X_REAL_IP'])) {
+        return $_SERVER['HTTP_X_REAL_IP'];
+    } else {
+        return $_SERVER['REMOTE_ADDR']; // Fallback (could be ::1 or internal IP)
+    }
 }
+
+$clientIP = getClientIP();
+echo "User's IP: " . $clientIP;
