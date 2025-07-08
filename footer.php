676b3df15e851edf665fe9073c736c99ebb8785e
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-07-08 22:53:07 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,4 @@
+ <!-- ✅ Footer -->
+  <footer class="bg-slate-800 text-center text-sm text-slate-400 py-6 mt-10 border-t border-slate-700">
+    © <?php echo date("Y"); ?> Movie Finder. All rights reserved.
+  </footer>
