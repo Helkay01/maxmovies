115abb7618dd29d89581671f2521f1e1202d9592
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-07-10 12:28:10 +0100

    Create link-finder.php

---
+++ link-finder.php
@@ -0,0 +1,22 @@
+<?php
+include "header.php";
+?>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script type="text/javascript">
+
+
+
+</script>
+
+
+
+<?php
+include "head.php";
+?>
+
+
+
+
