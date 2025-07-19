3309cd50d56d8e73e54d0e72c59c3de2110dda45
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-07-19 10:53:59 +0100

    Update header.php

--- header.php
+++ header.php
@@ -11,6 +11,31 @@ if (isset($_SERVER['HTTP_COOKIE'])) {
         unset($_COOKIE[$name]);
     }
 }
+
+
+
+//IP
+function getClientIp() {
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        return trim($ips[0]); // First IP is the real client IP
+    }
+
+    return $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
+}
+
+$ip = getClientIp();
+
+$reader = new Reader('GeoLite2-City.mmdb');
+$record = $reader->city($ip);
+		
+// Extract data
+$timezone = $record->location->timeZone;
+$country = $record->country->name;
+
+
+
+    
 ?>
 
 
