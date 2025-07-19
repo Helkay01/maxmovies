6dd2209465daad9bc1b5d2aa49e0733f389c4424
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-07-19 14:41:26 +0100

    Update header.php

--- header.php
+++ header.php
@@ -75,21 +75,96 @@ echo '
     }
   </style>
 
-
-  
-<script>
-     
-</script>
-
-
-
 ';
 
 
 }
 else {
-    echo '<script src="https://cdn.tailwindcss.com"></script>';
+    echo '
+    	<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		  <meta charset="UTF-8" />
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		  <title>Movie Finder</title>
+		
+		  <script src="https://fpyf8.com/88/tag.min.js" data-zone="157609" async data-cfasync="false"></script>
+		  <script src="https://cdn.tailwindcss.com"></script>';
+
+  
+		  <!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-R9QCCJ62Y8"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag("js", new Date());
+		
+		  gtag("config", "G-R9QCCJ62Y8");
+		</script>
+		  
+		  
+		  <style>
+		    body {
+		      background-color: #0f172a;
+		    }
+		    ::placeholder {
+		      color: #94a3b8;
+		    }
+		    .glass {
+		      background: rgba(255, 255, 255, 0.05);
+		      backdrop-filter: blur(10px);
+		    }
+		  </style>
+
+	';
 }
-    
+else {
+	echo '
+
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		  <meta charset="UTF-8" />
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		  <title>Movie Finder</title>
+		
+		  <script src="https://fpyf8.com/88/tag.min.js" data-zone="157609" async data-cfasync="false"></script>
+		  <script src="https://cdn.tailwindcss.com"></script>';
+
+  
+		  <!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-R9QCCJ62Y8"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag("js", new Date());
+		
+		  gtag("config", "G-R9QCCJ62Y8");
+		</script>
+		  
+		  
+		  <style>
+		    body {
+		      background-color: #0f172a;
+		    }
+		    ::placeholder {
+		      color: #94a3b8;
+		    }
+		    .glass {
+		      background: rgba(255, 255, 255, 0.05);
+		      backdrop-filter: blur(10px);
+		    }
+		  </style>
+
+	
+	';
+
+}
+
+
+
+
+
+
 
 ?>
