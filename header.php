dda21554639c4f230b91a7a7095a7f1903fc2c7c
Helkay01 <83680190+Helkay01@users.noreply.github.com>
2025-07-20 12:07:36 +0100

    Update header.php

--- header.php
+++ header.php
@@ -85,7 +85,42 @@ if($country === "Nigeria") {
    		<script src="https://fpyf8.com/88/tag.min.js" data-zone="157609" async data-cfasync="false"></script>
      		<script src="https://fpyf8.com/88/tag.min.js" data-zone="157609" async data-cfasync="false"></script>
  
- 
+
+
+		<div style="color: white">
+  			<a class="mfinder" href="/watch.php?t=The Rule of Jenny Pen&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/the-rul-of-jenny-pen-hollywood-movie.webp">Link</a>
+     			<a class="mfinder" href="/watch.php?t=Bride Hard&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/bride-hard-hollywood-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=Mr. Burton&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/mr-burton-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=The Salt Path&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/the-salt-path-hollywood-movie.webp">Link</a>
+      			<a class="mfinder" href="/watch.php?t=Eden&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/eden-hollywood-movie.jpg">Link</a>
+	 		<a class="mfinder" href="/watch.php?t=Megan 2.0&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/megan-2.0-hollywood-movie.webp">Link</a>
+    			<a class="mfinder" href="/watch.php?t=How to train your dragon&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/how-to-train-your-dragon-hollywood-movie.webp">Link</a>
+       			<a class="mfinder" href="/watch.php?t=Almost Cops&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/almost-cops-foreign-movie.webp">Link</a>
+	  		<a class="mfinder" href="/watch.php?t=Soverign&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/sovereignity-hollywood-movie.webp">Link</a>
+     			<a class="mfinder" href="/watch.php?t=Bridge&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/brick-foreign-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/madeas-destination-wedding-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=Medea Destination wedding&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/madeas-destination-wedding-hollywood-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=Everthing going to be great&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/everythings-going-to-be-great-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=ziam&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/ziam-thailand-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=The unholy trinity&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/the-unholy-trinity-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=Pretty Things&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/pretty-thing-hollywood-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=the old gaurd 2&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/the-old-guard-2-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=heads of state&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/heads-of-state-hollywood-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=guns up&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/guns-up-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=off the grid&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/off-the-grid-hollywood-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=tornado&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/tornado-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=ice road: vengeance&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/ice-road-vengeance-hollywood-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=bring her back&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/bring-her-back-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=ballerina&y=2025&img=https://nkiri.com/wp-content/uploads/2025/07/ballerina-hollywood-movie.jpg">Link</a>
+			<a class="mfinder" href="/watch.php?t=thunderbolts&y=2025&img=https://nkiri.com/wp-content/uploads/2025/06/thunderbolts-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=ritual&y=2025&img=https://nkiri.com/wp-content/uploads/2025/06/the-ritual-hollywood-movie.webp">Link</a>
+			<a class="mfinder" href="/watch.php?t=i dont understand&y=2025&img=https://nkiri.com/wp-content/uploads/2025/06/i-dont-understand-you-hollywood-movie.webp">Link</a>
+   			<a class="mfinder" href="/watch.php?t=the last rodeo&y=2025&img=https://nkiri.com/wp-content/uploads/2025/06/the-last-rodeo-hollywood-movie.webp">Link</a>
+			
+
+  		</div>
+
+   		
  	';
 }
 else {
